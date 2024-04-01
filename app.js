b802077d8b9654cee2ba38d2b7cd9fa47accd641
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-01 03:03:12 -0600

    regex for lowercase and whitespace strings

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let message = "";
+let lowerCaseRegex = /^(?:\s*[a-z]+\s*)?$/;
 
 function encrypt() {
-    message = document.getElementById('input-mensaje').value;
+    message = document.getElementById('input-mensaje').value.trim();
+    /*
+    Create an if else or something related to verify the regex or display an alert
+    */
 };
