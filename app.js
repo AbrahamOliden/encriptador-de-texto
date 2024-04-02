820d07686cade5aa06ed4d46352d0201849e2748
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-01 19:13:44 -0600

    lowercase regex implemented

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 let message = "";
+let lowerCaseRegex = /^(?:\s*[a-z]+\s*)?$/;
 
 function encrypt() {
     message = document.getElementById('input-mensaje').value;
+    console.log(lowerCaseRegex.test(message));
+    /*
+    Create an if else or something related to verify the regex or display an alert
+    */
 };
